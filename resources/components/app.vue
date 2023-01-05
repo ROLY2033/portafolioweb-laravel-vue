240e753ba2bd8b94a2d1237d1a8ce1380a1ebd3d
= <=>
2023-01-05 14:20:29 -0500

    agregando la configuracion de vue js con vite

---
+++ resources/components/app.vue
@@ -0,0 +1,6 @@
+<template>
+		<h1>
+            mi primer component para el portafolio con laravel & vue
+        </h1>
+
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+// no coloar en ./ solo / para la ruta inicial
+import app from '/resources/components/app.vue'
+
+createApp(app).mount("#app")
